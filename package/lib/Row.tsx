a484872d395ca5945f26a9107acc11c80fa8c807
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-17 15:36:05 -0500

    Rename to DeleteButton

--- package/lib/Row.tsx
+++ package/lib/Row.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DeleteButton from './_deleteButton'
+import DeleteButton from './DeleteButton'
 import { RowComponent } from './props'
 
 const Row: RowComponent = props => {
--- package/lib/inputs/array.tsx
+++ package/lib/inputs/array.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEventHandler } from 'react'
 import arraySchema from '../arraySchema'
-import DeleteButton from '../_deleteButton'
+import DeleteButton from '../DeleteButton'
 import getSubName from '../getSubName'
 import getValidInput from '../getValidInput'
 import { Input, InputComponent, ControlledPropsOnChange, RowPropsWithoutChildrenOnDelete, OnSelectedInputChange, SelectedInput } from '../props'

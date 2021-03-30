74c766d10b2f25a4f73f0527a4573c350b4ed5ca
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-30 14:44:56 -0500

    Seperate folders
    Seperate folders for packages and demo

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,4 +1,6 @@
 // Based on example in https://github.com/netlify/next-on-netlify/tree/master#1-set-nextjs-target-to-serverless
-module.exports = {
+const withTranspile = require('next-transpile-modules')
+
+module.exports = withTranspile(['react-json-input'])({
   target: 'serverless'
-}
+})
--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,10 +1,10 @@
 import { JSONSchema7 } from 'json-schema'
 import { ChangeEventHandler, FC, useCallback, useState } from 'react'
-import JsonInput from '../package/lib/JsonInput'
+import JsonInput from 'react-json-input/lib/JsonInput'
 import JsonPretty from 'react-json-pretty'
 import 'react-json-pretty/themes/acai.css'
-import valueFromSchema from '../package/lib/valueFromSchema'
-import defaultInputs from '../package/lib/defaultInputs'
+import valueFromSchema from 'react-json-input/lib/valueFromSchema'
+import defaultInputs from 'react-json-input/lib/defaultInputs'
 
 const schema: JSONSchema7 = {
   type: 'array',

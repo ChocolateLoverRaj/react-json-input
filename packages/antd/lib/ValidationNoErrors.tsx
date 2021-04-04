1ec0e49d26df3236ac782d9917e6839431d5b21e
Rajas Paranjpe <paranjperajas@gmail.com>
2021-04-04 18:50:09 -0500

    ValidationNoErrors

---
+++ packages/antd/lib/ValidationNoErrors.tsx
@@ -0,0 +1,6 @@
+import { ValidationNoErrorsComponent } from '@chocolateloverraj/react-json-input/dist/props'
+import { CheckOutlined } from '@ant-design/icons'
+
+const ValidationNoErrors: ValidationNoErrorsComponent = () => <CheckOutlined />
+
+export default ValidationNoErrors
--- packages/antd/lib/index.tsx
+++ packages/antd/lib/index.tsx
@@ -1,8 +1,14 @@
 import { Props } from '@chocolateloverraj/react-json-input/dist/props'
-import {JsonInput} from '@chocolateloverraj/react-json-input'
+import { JsonInput } from '@chocolateloverraj/react-json-input'
+import ValidationNoErrors from './ValidationNoErrors'
 
 const JsonInputAntd = <T extends unknown = any>(props: Partial<Props<T>>): JSX.Element => {
-  return <JsonInput {...props} />
+  return (
+    <JsonInput
+      ValidationNoErrors={ValidationNoErrors}
+      {...props}
+    />
+  )
 }
 
 export default JsonInputAntd

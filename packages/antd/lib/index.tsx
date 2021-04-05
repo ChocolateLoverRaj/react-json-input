839418be9a43d343aecc6226e82471e965567037
Rajas Paranjpe <paranjperajas@gmail.com>
2021-04-05 10:28:05 -0500

    Checkbox

--- packages/antd/lib/index.tsx
+++ packages/antd/lib/index.tsx
@@ -1,13 +1,29 @@
-import { Props } from '@chocolateloverraj/react-json-input/dist/props'
-import { JsonInput } from '@chocolateloverraj/react-json-input'
+import { Props, Input } from '@chocolateloverraj/react-json-input/dist/props'
+import { JsonInput, defaultInputs } from '@chocolateloverraj/react-json-input'
 import ValidationNoErrors from './ValidationNoErrors'
 import ValidationErrors from './ValidationErrors'
 import Select from './Select'
 import Option from './Option'
 import DeleteButton from './DeleteButton'
 import Button from './Button'
+import InputBoolean from './InputBoolean'
 
 const JsonInputAntd = <T extends unknown = any>(props: Partial<Props<T>>): JSX.Element => {
+  const { inputs } = props
+
+  const antdInputs = ((inputs ?? defaultInputs) as Input[])
+    .map(input => {
+      switch (input.name) {
+        case 'boolean':
+          return {
+            ...input,
+            Component: InputBoolean
+          }
+        default:
+          return input
+      }
+    })
+
   return (
     <JsonInput
       ValidationNoErrors={ValidationNoErrors}
@@ -17,6 +33,7 @@ const JsonInputAntd = <T extends unknown = any>(props: Partial<Props<T>>): JSX.E
       DeleteButton={DeleteButton}
       Button={Button}
       {...props}
+      inputs={antdInputs}
     />
   )
 }

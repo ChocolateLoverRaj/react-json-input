a8e216e1a66aff8104a4099f25ee1cc2f5ce3a13
Rajas Paranjpe <paranjperajas@gmail.com>
2021-04-04 19:09:54 -0500

    ValidationErrors

--- packages/antd/lib/index.tsx
+++ packages/antd/lib/index.tsx
@@ -1,11 +1,13 @@
 import { Props } from '@chocolateloverraj/react-json-input/dist/props'
 import { JsonInput } from '@chocolateloverraj/react-json-input'
 import ValidationNoErrors from './ValidationNoErrors'
+import ValidationErrors from './ValidationErrors'
 
 const JsonInputAntd = <T extends unknown = any>(props: Partial<Props<T>>): JSX.Element => {
   return (
     <JsonInput
       ValidationNoErrors={ValidationNoErrors}
+      ValidationErrors={ValidationErrors}
       {...props}
     />
   )
